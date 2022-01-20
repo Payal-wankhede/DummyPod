8666dba9d71a7009114565079df783ff8e23cdd1
Payal <p.wankhede@easternenterprise.com>
2022-01-20 18:33:15 +0530

    podspec file

---
+++ DummyLibrary/Dummy.swift
@@ -0,0 +1,20 @@
+//
+//  Dummy.swift
+//  DummyLibrary
+//
+//  Created by Payal Wankhede on 20/01/22.
+//
+
+import Foundation
+import UIKit
+
+public struct Dummy {
+  // mutating public var email: String?
+  // mutating public var name: String?
+    
+    public func updateData(email: String, name: String) {
+       // self.email = email
+        //self.name = name
+    }
+}
+
