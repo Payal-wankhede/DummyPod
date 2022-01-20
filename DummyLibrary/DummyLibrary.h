2528adc6ae8202897530016c830a35abbe6f1ea6
Payal <p.wankhede@easternenterprise.com>
2022-01-20 17:11:22 +0530

    Initial Commit

---
+++ DummyLibrary/DummyLibrary.h
@@ -0,0 +1,18 @@
+//
+//  DummyLibrary.h
+//  DummyLibrary
+//
+//  Created by Payal Wankhede on 20/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DummyLibrary.
+FOUNDATION_EXPORT double DummyLibraryVersionNumber;
+
+//! Project version string for DummyLibrary.
+FOUNDATION_EXPORT const unsigned char DummyLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummyLibrary/PublicHeader.h>
+
+
